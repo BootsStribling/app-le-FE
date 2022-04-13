969b06d5e9de3c98155fd0b12585f6d661a4781b
RMo <morici23@gmail.com>
2022-04-13 15:57:29 -0400

    Show 7 days in the daylist

--- src/components/DayList/DayList.jsx
+++ src/components/DayList/DayList.jsx
@@ -1,7 +1,8 @@
-import { Link } from 'react-router-dom'
 import DayCard from '../DayCard/DayCard';
+import { useState, useEffect } from 'react';
 // The purpose of this component is to list all of the dates for the current user.
 const DayList = (props) => {
+
   return (
     <>
       {props.days
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,13 +4,16 @@ import DayDetails from '../DayDetails/DayDetails'
 import { useEffect, useState } from 'react'
 // The purpose of this landing is to set the current date and sho
 const Landing = ({ user, days, createDay, updateCurrentDay, currentDay, updateDay }) => {
-
+  const [week, setWeek] = useState([...days])
+  useEffect(()=>{
+    setWeek(days.reverse().slice(0,7))
+  }, [days])
 
   return (
     <main className="landing">
       {user ? 
         <>
-          <DayList updateCurrentDay={updateCurrentDay} days={days} />
+          <DayList updateCurrentDay={updateCurrentDay} days={week} />
           <DayDetails updateDay={updateDay} updateCurrentDay={updateCurrentDay} currentDay={currentDay} />
           <button onClick={createDay}>A Brand New Day</button>
         </>
