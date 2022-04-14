6cc1cb6f32afe6c86b528a37f40cd7ea4f7ae142
RMo <morici23@gmail.com>
2022-04-13 21:15:20 -0400

    Day squares change color depending on stand up / stand down / jobs apps

--- src/components/DayCard/DayCard.jsx
+++ src/components/DayCard/DayCard.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 // the purpose of this component is to show the day as a clicable div
 const DayCard = (props) => {
+  const [colorClass, setColorClass] = useState()
+
+  useEffect(()=> {
+    let counter = 0
+    if(props.day.stand_up) counter++
+    if(props.day.stand_down) counter++
+    if(props.day.jerbs.length) counter++
+
+    if(counter === 0) setColorClass('daysq')
+    if(counter === 1) setColorClass('daysq_1')
+    if(counter === 2) setColorClass('daysq_2')
+    if(counter === 3) setColorClass('daysq_3')
+
+  }, [props.day])
+
   return (
-    <button className="daysq" onClick={()=> props.updateCurrentDay(props.day)}></button>
+    <button className={colorClass} onClick={()=> props.updateCurrentDay(props.day)}></button>
   );
 }
  
